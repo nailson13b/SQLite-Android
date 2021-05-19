0632d349ecd107f8db01519cf6a55d8bec960a92
nailson13b <nailson.oliveira@outlook.com>
2021-05-19 19:13:17 -0300

    helperdb updated

--- app/src/main/java/com/everis/listadecontatos/helper/HelperDB.kt
+++ app/src/main/java/com/everis/listadecontatos/helper/HelperDB.kt
@@ -1,16 +1,18 @@
 package com.everis.listadecontatos.helper
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.everis.listadecontatos.feature.listacontatos.model.ContatosVO
 
 class HelperDB(
-    context: Context?,
+    context: Context?
 ) : SQLiteOpenHelper(context, NOME_BANCO, null, VERSAO_ATUAL) {
 
     companion object{
         private val NOME_BANCO = "contato.db"
-        private val VERSAO_ATUAL = 1
+        private val VERSAO_ATUAL = 2
     }
 
     val TABLE_NAME = "contato"
@@ -36,5 +38,36 @@ class HelperDB(
 
         }
         onCreate(db)
+    }
+
+    fun buscarContatos(busca: String): List<ContatosVO>{
+        val db = readableDatabase ?: return mutableListOf()
+        val lista = mutableListOf<ContatosVO>()
+        var where = "$COLUMNS_NOME LIKE ?"
+        var buscaPercentual = arrayOf("%$busca%")
+        var cursor = db.query(TABLE_NAME, null, where, buscaPercentual, null, null, null)
+        if(cursor == null){
+            db.close()
+            return mutableListOf()
+        }
+        while (cursor.moveToNext()){
+            var contato = ContatosVO(
+                cursor.getInt(cursor.getColumnIndex(COLUMNS_ID)),
+                cursor.getString(cursor.getColumnIndex(COLUMNS_NOME)),
+                cursor.getString(cursor.getColumnIndex(COLUMNS_TELEFONE))
+            )
+            lista.add(contato)
+        }
+        db.close()
+        return lista
+    }
+
+    fun salvarContato(contato: ContatosVO){
+        val db  = writableDatabase ?: return
+        var content = ContentValues()
+        content.put(COLUMNS_NOME,contato.nome)
+        content.put(COLUMNS_TELEFONE,contato.telefone)
+        db.insert(TABLE_NAME, null, content)
+        db.close()
     }
 }
